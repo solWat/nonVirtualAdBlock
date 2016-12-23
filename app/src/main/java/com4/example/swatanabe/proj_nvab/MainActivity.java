e49e7f479b4206efa034fcec098a1ab63951f8a6
sWat <s.Watanabe@watanabesakai-no-MacBook-Air.local>
2016-12-23 23:44:08 +0900

    要らないコメントをカット．

--- app/src/main/java/com4/example/swatanabe/proj_nvab/MainActivity.java
+++ app/src/main/java/com4/example/swatanabe/proj_nvab/MainActivity.java
@@ -21,21 +21,6 @@ import java.util.ArrayList;
 
 public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
     private CameraBridgeViewBase mCameraView;
-    //private Mat mOutputFrame;
-
-//    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-//        @Override
-//        public void onManagerConnected(int status) {
-//            switch (status) {
-//                case LoaderCallbackInterface.SUCCESS:
-//                    mCameraView.enableView();
-//                    break;
-//                default:
-//                    super.onManagerConnected(status);
-//                    break;
-//            }
-//        }
-//    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -115,46 +100,3 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
 //        return inputFrame.rgba();
     }
 }
-
-//    @Override
-//    public void onPause() {
-//        if (mCameraView != null) {
-//            mCameraView.disableView();
-//        }
-//        super.onPause();
-//    }
-//
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_4, this, mLoaderCallback);
-//    }
-//
-//    @Override
-//    public void onDestroy() {
-//        super.onDestroy();
-//        if (mCameraView != null) {
-//            mCameraView.disableView();
-//        }
-//    }
-//
-//    @Override
-//    public void onCameraViewStarted(int width, int height) {
-//        // Mat(int rows, int cols, int type)
-//        // rows(行): height, cols(列): width
-//        mOutputFrame = new Mat(height, width, CvType.CV_8UC1);
-//    }
-//
-//    @Override
-//    public void onCameraViewStopped() {
-//        mOutputFrame.release();
-//    }
-//
-//    @Override
-//    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-//        // Cannyフィルタをかける
-//        Imgproc.Canny(inputFrame.gray(), mOutputFrame, 80, 100);
-//        // ビット反転
-//        Core.bitwise_not(mOutputFrame, mOutputFrame);
-//        return mOutputFrame;
-//    }
