8f8bca6a031a7ad93c54bbe96fbe43c0ddd58ea4
sWat <s.Watanabe@watanabesakai-no-MacBook-Air.local>
2016-12-23 21:10:38 +0900

    「OpenCV for Android入門 – カメラ編」（http://rest-term.com/archives/3010/）を参考に，OpenCVを使ったカメラアプリを実際に動かしてみる． これを使うと，プレビューがモノクロになり，輪郭が強調される．

--- app/src/main/java/com4/example/swatanabe/proj_nvab/MainActivity.java
+++ app/src/main/java/com4/example/swatanabe/proj_nvab/MainActivity.java
@@ -1,13 +1,84 @@
 package com4.example.swatanabe.proj_nvab;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+    private CameraBridgeViewBase mCameraView;
+    private Mat mOutputFrame;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    mCameraView.enableView();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCameraView = (CameraBridgeViewBase)findViewById(R.id.camera_view);
+        mCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        if (mCameraView != null) {
+            mCameraView.disableView();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_4, this, mLoaderCallback);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mCameraView != null) {
+            mCameraView.disableView();
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        // Mat(int rows, int cols, int type)
+        // rows(行): height, cols(列): width
+        mOutputFrame = new Mat(height, width, CvType.CV_8UC1);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mOutputFrame.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        // Cannyフィルタをかける
+        Imgproc.Canny(inputFrame.gray(), mOutputFrame, 80, 100);
+        // ビット反転
+        Core.bitwise_not(mOutputFrame, mOutputFrame);
+        return mOutputFrame;
     }
 }
